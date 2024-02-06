7a67749caa12180560fe765db29ba808dcdca9cf
Oleg Jukovec <oleg.jukovec@tarantool.org>
2024-02-06 15:33:46 +0300

    api: add the Scheduler interface
    The interface allows to schedule update events.
    
    Part of #2

---
+++ scheduler.go
@@ -0,0 +1,17 @@
+package discovery
+
+import (
+	"context"
+)
+
+// Scheduler is an interface that allows to schedule update events. An
+// implementation of the interface should generate events, a client should
+// wait on the `Wait()` method for a next event.
+type Scheduler interface {
+	// Wait blocks until a next event. It returns `nil` if an event occurs or
+	// an `error` if the context expired or the Scheduler implementation has
+	// been stopped.
+	Wait(ctx context.Context) error
+	// Stop stops an event generation.
+	Stop()
+}
