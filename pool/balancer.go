3e23724c370dabd3bbe0f7a5d4a544414a43d948
psergee <p_sergee@mail.ru>
2024-02-20 16:31:45 +0300

    api: implement round-robin balancer
    Closes #17

---
+++ pool/balancer.go
@@ -0,0 +1,13 @@
+// Package pool implements a pool of connections for Tarantool 3.0.
+package pool
+
+import "github.com/tarantool/go-discovery"
+
+type Balancer interface {
+	// Add adds an instance to balancer. Returns false if an instance was not added.
+	Add(instance discovery.Instance) bool
+	// Remove removes an instance from the balancer by name.
+	Remove(instanceName string)
+	// Next return a next instance according to the balancer logic.
+	Next(mode discovery.Mode) (string, bool)
+}
