751e835f759c41dc0dba7cc2d647c1bddcd7cb06
DerekBum <alextruewestern@gmail.com>
2024-02-28 20:28:27 +0300

    tests: move common observer mock to the separate file

--- subscriber/schedule_test.go
+++ subscriber/schedule_test.go
@@ -59,34 +59,6 @@ func (d *mockDiscoverer) Discovery(ctx context.Context) ([]discovery.Instance, e
 	}
 }
 
-type mockObserver struct {
-	eventCnt     atomic.Int32
-	recentEvents atomic.Pointer[[]discovery.Event]
-
-	errCnt    atomic.Int32
-	recentErr error
-
-	errWg sync.WaitGroup
-}
-
-func newMockObserver() *mockObserver {
-	obs := &mockObserver{}
-	obs.recentEvents.Store(&[]discovery.Event{})
-	obs.errWg.Add(1)
-	return obs
-}
-
-func (o *mockObserver) Observe(events []discovery.Event, err error) {
-	if err != nil {
-		o.errCnt.Add(1)
-		o.recentErr = err
-		o.errWg.Done()
-	} else {
-		o.eventCnt.Add(1)
-		o.recentEvents.Store(&events)
-	}
-}
-
 func TestNewSchedule_NilScheduler(t *testing.T) {
 	schedule := subscriber.NewSchedule(nil, nil)
 	assert.NotNil(t, schedule)
