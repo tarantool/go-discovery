2645ad3cc34d5d495c4972e789f80cf03c3a7df4
DerekBum <alextruewestern@gmail.com>
2024-02-28 20:17:20 +0300

    api: implement `Filter` subscriber
    `Filter` subscriber is a wrapper to any other `Subscriber`.
    It allows filtering the stream of events from the inner `Subscriber`.
    Only nodes with suitable configurations are sent to the observer.
    
    Closes #8

--- example_test.go
+++ example_test.go
@@ -167,3 +167,161 @@ func Example_subscriber_Schedule_Etcd_canceled() {
 	// Subscribe error: context canceled
 	// Done.
 }
+
+func Example_subscriber_Filter() {
+	cluster := integration.NewLazyCluster()
+	defer cluster.Terminate()
+
+	etcd, err := clientv3.New(clientv3.Config{
+		Endpoints: cluster.EndpointsV3(),
+	})
+	if err != nil {
+		fmt.Println("Unable to start etcd client:", err)
+		return
+	}
+	defer etcd.Close()
+
+	schedule := subscriber.NewSchedule(scheduler.NewEtcdWatch(etcd, "foo"),
+		discoverer.NewEtcd(etcd, "foo"))
+
+	filters := []discovery.Filter{
+		discovery.FilterFunc(func(inst discovery.Instance) bool {
+			return inst.Name == "zoo"
+		}),
+		discovery.FilterFunc(func(inst discovery.Instance) bool {
+			return inst.Group == "foo"
+		}),
+	}
+	filter := subscriber.NewFilter(schedule, filters...)
+
+	obs := newExampleObserver()
+	obs.wgEvent.Add(2)
+
+	err = filter.Subscribe(context.Background(), obs)
+	if err != nil {
+		fmt.Println("Subscribe error:", err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), etcdTimeout)
+	_, err = etcd.Put(ctx, "foo/config/key", `
+database:
+  mode: ro
+groups:
+  foo:
+    replicasets:
+      bar:
+        instances:
+          zoo:
+            iproto:
+              listen:
+              - uri: localhost:3011
+              - uri: localhost:3012
+                params:
+                  transport: ssl
+              - uri: localhost:3013
+                params:
+                  transport: plain
+            roles: [crud]
+            roles_cfg:
+              tags:
+              - any
+              - bar
+              - 3
+            app:
+              cfg:
+                tags:
+                - foo
+                - bar
+  foo2:
+    replicasets:
+      bar:
+        instances:
+          zoo2:
+            iproto:
+              listen:
+              - uri: localhost:3011
+              - uri: localhost:3012
+                params:
+                  transport: ssl
+              - uri: localhost:3013
+                params:
+                  transport: plain
+            roles: [crud]
+            roles_cfg:
+              tags:
+              - any
+              - bar
+              - 3
+            app:
+              cfg:
+                tags:
+                - foo2
+                - bar
+`)
+	cancel()
+
+	if err != nil {
+		fmt.Println("Put error:", err)
+		return
+	}
+
+	obs.wgEvent.Wait()
+	filter.Unsubscribe(obs)
+
+	fmt.Println("Done.")
+
+	// Output:
+	// An event found.
+	// An event found.
+	// Type: add
+	// Group: foo
+	// Replicaset: bar
+	// Name: zoo
+	// Mode: ro
+	// Endpoints: [{localhost:3011 plain} {localhost:3012 ssl} {localhost:3013 plain}]
+	// Roles: [crud]
+	// RolesTags: [any bar 3]
+	// AppTags: [foo bar]
+	// Error from the observer: unsubscribed
+	// Done.
+}
+
+func Example_subscriber_Filter_Etcd_canceled() {
+	cluster := integration.NewLazyCluster()
+	defer cluster.Terminate()
+
+	etcd, err := clientv3.New(clientv3.Config{
+		Endpoints: cluster.EndpointsV3(),
+	})
+	if err != nil {
+		fmt.Println("Unable to start etcd client:", err)
+		return
+	}
+	defer etcd.Close()
+
+	schedule := subscriber.NewSchedule(scheduler.NewEtcdWatch(etcd, "foo"),
+		discoverer.NewEtcd(etcd, "foo"))
+
+	filters := []discovery.Filter{
+		discovery.FilterFunc(func(inst discovery.Instance) bool {
+			return inst.Name == "zoo"
+		}),
+	}
+	filter := subscriber.NewFilter(schedule, filters...)
+
+	obs := newExampleObserver()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	err = filter.Subscribe(ctx, obs)
+	if err != nil {
+		fmt.Println("Subscribe error:", err)
+	}
+	fmt.Println("Done.")
+
+	// Output:
+	// Subscribe error: context canceled
+	// Done.
+}
