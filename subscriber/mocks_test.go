a16524f1c5e217ef118628f5d57eb4d9dd441b53
DerekBum <alextruewestern@gmail.com>
2024-03-19 02:28:02 +0300

    tests: move common subscriber mock to the mocks file

--- subscriber/mocks_test.go
+++ subscriber/mocks_test.go
@@ -1,10 +1,12 @@
 package subscriber_test
 
 import (
+	"context"
 	"sync"
 	"sync/atomic"
 
 	"github.com/tarantool/go-discovery"
+	"github.com/tarantool/go-discovery/subscriber"
 )
 
 type mockObserver struct {
@@ -33,4 +35,56 @@ func (o *mockObserver) Observe(events []discovery.Event, err error) {
 		o.eventCnt.Add(1)
 		o.recentEvents.Store(&events)
 	}
+}
+
+type mockSubscriber struct {
+	subCnt   atomic.Int32
+	unsubCnt atomic.Int32
+	state    chan discovery.Observer
+
+	eventsReturn []discovery.Event
+}
+
+func newMockSubscriber() *mockSubscriber {
+	state := make(chan discovery.Observer, 1)
+	state <- nil
+
+	return &mockSubscriber{
+		state: state,
+	}
+}
+
+func (s *mockSubscriber) Subscribe(ctx context.Context,
+	observer discovery.Observer) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+	}
+
+	state := <-s.state
+	if state != nil {
+		s.state <- state
+		return subscriber.ErrSubscriptionExists
+	}
+	state = observer
+	s.state <- state
+
+	s.subCnt.Add(1)
+
+	if s.eventsReturn != nil {
+		observer.Observe(s.eventsReturn, nil)
+	}
+	return nil
+}
+
+func (s *mockSubscriber) Unsubscribe(observer discovery.Observer) {
+	state := <-s.state
+	if state == observer && observer != nil {
+		s.unsubCnt.Add(1)
+		observer.Observe(nil, discovery.ErrUnsubscribe)
+
+		state = nil
+	}
+	s.state <- state
 }
